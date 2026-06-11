4e2e066e198bc3452a36a37c24021bcd187424e8
GozzyGit <lee.gosling876@outlook.com>
2026-06-11 21:19:09 +0100

    .

--- function_app.py
+++ function_app.py
@@ -1,7 +1,7 @@
 import azure.functions as func
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
-
+   
 @app.route(route="hello")
 def hello(req: func.HttpRequest) -> func.HttpResponse:
     return func.HttpResponse("Hello from Azure Functions!")
